c770289be981586faa2852ba7cfbb70a13adb0a3
WellnessKerux <arv_que_aalcazaren@hotmail.com>
2025-06-22 11:25:35 +0800

    Create index.jsx

---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import VetWellnessHub from './App';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<React.StrictMode><VetWellnessHub /></React.StrictMode>);
