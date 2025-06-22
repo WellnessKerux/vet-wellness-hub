d17ac0b91b2c04f14f5d01a406dfaf92ddbc944c
WellnessKerux <arv_que_aalcazaren@hotmail.com>
2025-06-22 12:26:20 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
 import React from 'react';
 
 export default function VetWellnessHub() {
-  return (
-    <div>
-      <h1>Hello from Vet Wellness Hub!</h1>
-    </div>
-  );
+  return <h1>Hello from Vet Wellness Hub</h1>;
 }
